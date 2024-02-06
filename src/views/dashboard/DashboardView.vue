b3ad17d8b6ae7a94b8eca5eaf0ef37657b7ccfaf
“iris <chidy76071919@hotmail.com>
2024-02-06 22:03:51 +0800

    後台更新登入驗證

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -9,7 +9,33 @@
   </template>
 
 <script>
+import axios from 'axios'
+const { VITE_API_URL } = import.meta.env
+
 export default {
+  methods: {
+    checkLogin () {
+      // 驗證登入
+      const url = `${VITE_API_URL}/api/user/check`
+      axios.post(url)
+        .then((res) => {
+          console.log('驗證成功:', res.data.success)
+        })
+        .catch((err) => {
+          alert(err.response.data.message)
+          this.$router.push('/login')
+        })
+    }
+  },
+  mounted () {
+    // 將token存到cookie以進行驗證
+    const myCookie = document.cookie.replace(
+      /(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/,
+      '$1'
+    )
+    axios.defaults.headers.common.Authorization = myCookie
+    this.checkLogin()
+  }
 }
 </script>
 
