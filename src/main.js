ba55993f430e2538207155dc6efa032b41331000
“iris <chidy76071919@hotmail.com>
2024-03-02 16:08:38 +0800

    引入bootstrap scss

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import 'bootstrap/scss//bootstrap.scss'
 import 'sweetalert2/src/sweetalert2.scss'
-
+import './assets/all.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
