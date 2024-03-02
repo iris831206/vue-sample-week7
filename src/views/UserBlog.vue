ef1c51fc8e935521440bb2f75b26158df8c270c2
“iris <chidy76071919@hotmail.com>
2024-03-02 17:41:15 +0800

    更新前台貼文頁面

--- src/views/UserBlog.vue
+++ src/views/UserBlog.vue
@@ -1,10 +1,60 @@
 <template>
     <h1>這是貼文頁面</h1>
-
+    <div class="container">
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+      <template v-for="article in articles" :key="article.id">
+        <div class="col" v-if="article.isPublic">
+          <div class="card">
+            <img :src="article.imageUrl" class="card-img-top" />
+            <div class="card-body">
+              <h5 class="card-title">{{ article.title }}</h5>
+              <div v-html="article.description"></div>
+            </div>
+            <div class="card-footer">
+              <RouterLink
+                :to="`/user/article/${article.id}`"
+                v-if="article.isPublic"
+                class="btn btn-outline-primary"
+              >
+                文章頁面
+              </RouterLink>
+            </div>
+          </div>
+        </div>
+      </template>
+    </div>
+  </div>
   </template>
 
 <script>
+const { VITE_API_URL, VITE_API_PATH } = import.meta.env
+
 export default {
+  data () {
+    return {
+      articles: [],
+      isLoading: false,
+      isNew: false,
+      tempArticle: {}
+    }
+  },
+  methods: {
+    getArticles (page = 1) {
+      const api = `${VITE_API_URL}/api/${VITE_API_PATH}/articles?page=${page}`
+      this.isLoading = true
+      this.$http.get(api).then((response) => {
+        this.articles = response.data.articles
+        this.pagination = response.data.pagination
+        this.isLoading = false
+      }).catch((error) => {
+        this.isLoading = false
+        alert(error.response.data.message)
+      })
+    }
+  },
+  mounted () {
+    this.getArticles()
+  }
 }
 </script>
 
