f66be3ed02eac23daea04e25e8a312f5289e63c0
“iris <chidy76071919@hotmail.com>
2024-03-03 12:04:44 +0800

    更新後台貼文管理頁面

--- src/views/dashboard/ArticleView.vue
+++ src/views/dashboard/ArticleView.vue
@@ -40,83 +40,8 @@
         </tr>
       </tbody>
     </table>
-    <!-- ArticleModal -->
-    <div class="modal fade" id="articleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" ref="articleModal">
-      <div class="modal-dialog modal-xl" role="document">
-        <div class="modal-content border-0">
-          <div class="modal-header bg-dark text-white">
-            <h5 class="modal-title" id="exampleModalLabel">
-              <span v-if="isNew">新增貼文</span>
-              <span v-else>編輯貼文</span>
-            </h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <div class="row">
-              <div class="col-sm-4">
-                <div class="mb-3">
-                  <label for="title" class="form-label">標題</label>
-                  <input type="text" class="form-control" id="title" v-model="tempArticle.title" placeholder="請輸入標題" />
-                </div>
-                <div class="mb-3">
-                  <label for="image" class="form-label">輸入圖片網址</label>
-                  <input type="text" class="form-control" id="image" v-model="tempArticle.imageUrl" placeholder="請輸入圖片連結" />
-                </div>
-                <div class="mb-3">
-                  <label for="author" class="form-label">作者</label>
-                  <input type="text" class="form-control" id="author" v-model="tempArticle.author" placeholder="請輸入標題" />
-                </div>
-                <div class="mb-3">
-                  <label for="create_at">文章建立日期</label>
-                  <input type="date" class="form-control" id="create_at" v-model="create_at" />
-                </div>
-              </div>
-              <div class="col-sm-8">
-                <label for="tag" class="form-label">標籤</label>
-                <div class="row gx-1 mb-3">
-                  <div class="col-md-2 mb-1" v-for="(label, key) in tempArticle.tag" :key="key">
-                    <div class="input-group input-group-sm">
-                      <input type="text" class="form-control form-control" id="tag" v-model="tempArticle.tag[key]" placeholder="請輸入標籤" />
-                      <button type="button" class="btn btn-outline-danger" @click="tempArticle.tag.splice(key, 1)">
-                        <i class="bi bi-x"></i>
-                      </button>
-                    </div>
-                  </div>
-                  <!-- <div class="col-md-2 mb-1" v-if="tempArticle.tag[tempArticle.tag.length - 1] || !tempArticle.tag.length">
-                    <button class="btn btn-outline-primary btn-sm d-block w-100" type="button" @click="tempArticle.tag.push('')">
-                      新增標籤
-                    </button>
-                  </div> -->
-                </div>
-                <div class="mb-3">
-                  <label for="description" class="form-label">文章描述</label>
-                  <textarea type="text" class="form-control" id="description" v-model="tempArticle.description" placeholder="請輸入文章描述"></textarea>
-                </div>
-                <div class="mb-3">
-                  <ckeditor :editor="editor" :config="editorConfig" v-model="tempArticle.content"></ckeditor>
-                </div>
-                <div class="mb-3">
-                  <div class="form-check">
-                    <input class="form-check-input" type="checkbox" v-model="tempArticle.isPublic" id="isPublic" />
-                    <label class="form-check-label" for="isPublic">
-                      是否公開
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-              取消
-            </button>
-            <button type="button" class="btn btn-primary" @click="updateArticle(tempArticle)">
-              確認
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ArticleModal ref="articleModal" :article="tempArticle" :is-new="isNew" @update-article="updateArticle">
+    </ArticleModal>
     <!-- ArticleModal -->
     <!-- DelModal -->
     <div class="modal fade" id="delModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" ref="delArticleModal">
@@ -146,9 +71,9 @@
 </template>
 
 <script>
+import ArticleModal from '@/components/ArticleModal.vue'
 import PaginationComponents from '@/components/PaginationComponents.vue'
 import { Modal } from 'bootstrap'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 
 const { VITE_API_URL, VITE_API_PATH } = import.meta.env
 
@@ -158,18 +83,8 @@ export default {
       articles: [],
       isLoading: false,
       isNew: false,
+      tempArticle: {},
       currentPage: 1,
-      tempArticle: {
-        tag: ['']
-      },
-      create_at: 0,
-      // 參考：https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/frameworks/vuejs-v3.html#editor
-      editor: ClassicEditor,
-      editorConfig: {
-        toolbar: ['heading', 'bold', 'italic', '|', 'link']
-      },
-      articleModal: null,
-      delArticleModal: null,
       pagination: {}
     }
   },
@@ -199,7 +114,7 @@ export default {
         this.tempArticle = { ...item }
         this.isNew = false
       }
-      this.articleModal.show()
+      this.$refs.articleModal.openModal()
     },
     openDelArticleModal (item) {
       this.tempArticle = { ...item }
@@ -216,7 +131,7 @@ export default {
       }
       this.$http[httpMethod](api, { data: this.tempArticle }).then((response) => {
         this.isLoading = false
-        this.articleModal.hide()
+        this.$refs.articleModal.hideModal()
         this.getArticles(this.currentPage)
       }).catch((error) => {
         this.isLoading = false
@@ -267,10 +182,11 @@ export default {
     }
   },
   components: {
-    PaginationComponents
+    PaginationComponents,
+    ArticleModal
   },
   mounted () {
-    this.articleModal = new Modal(this.$refs.articleModal)
+    // this.articleModal = new Modal(this.$refs.articleModal)
     this.delArticleModal = new Modal(this.$refs.delArticleModal)
     this.getArticles()
   }
